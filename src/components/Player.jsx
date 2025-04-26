8f42dcb5e1945c3f3a8653aead4d774886d643fa
caporal404 <kevincode26@gmail.com>
2025-04-26 21:26:18 +0100

    Affichage correcte des statistiques des joueurs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,20 +3,29 @@ import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, Tooltip 
 import { usePlayers } from '../hooks/player-hooks';
 import '../styles/Player.css';
 
-const Player = ({ picture, weight, height, speed, strength, endurance, ...data }) => {
+const Player = ({ id, picture, weight, height, speed, strength, endurance, ...data }) => {
   const { setModifiedPlayer, removePlayer, goToNextSection } = usePlayers();
 
+  const MAX_VALUES = {
+    weight: 100, 
+    height: 220, 
+    speed: 30, 
+    strength: 100, 
+    endurance: 500
+  };
+
   const stats = [
-    { stat: 'Weight', value: weight, fullMark: 100 },
-    { stat: 'Height', value: height, fullMark: 220 },
-    { stat: 'Speed', value: speed, fullMark: 30 },
-    { stat: 'Strength', value: strength, fullMark: 100 },
-    { stat: 'Endurance', value: endurance, fullMark: 700 },
+    { stat: 'Weight', value: weight + ' Kg', percent: (weight / MAX_VALUES.weight) * 100 },
+    { stat: 'Height', value: height + ' cm', percent: (height / MAX_VALUES.height) * 100 },
+    { stat: 'Speed', value: speed + ' m/s', percent: (speed / MAX_VALUES.speed) * 100 },
+    { stat: 'Strength', value: strength + ' N', percent: (strength / MAX_VALUES.strength) * 100 },
+    { stat: 'Endurance', value: endurance + ' VO', percent: (endurance / MAX_VALUES.endurance) * 100 },
   ]
 
 
   return (
     <tr className="player">
+      <td className='id'>{id}</td>
       <td className='picture'>
         <img src={picture} alt="Photo du joueur" />
       </td>
@@ -27,29 +36,26 @@ const Player = ({ picture, weight, height, speed, strength, endurance, ...data }
       }
       <td className="stats">
         <RadarChart
-          cx="50%"
-          cy="50%"
-          outerRadius="80%"
-          width={100}
-          height={100}
+          width={280}
+          height={160}
           data={stats}
         >
           <PolarGrid />
           <PolarAngleAxis dataKey='stat' />
-          <PolarRadiusAxis />
+          <PolarRadiusAxis domain={[0, 100]} />
           <Radar 
-            name='Performance du joueur'
-            dataKey="value"
+            name='Valeur'
+            dataKey="percent"
             stroke='#8884d8'
             fill='#8884d8'
             fillOpacity={0.6}
           />
-          <Tooltip />
+          <Tooltip formatter={(value, name, props) => props.payload.value}/>
         </RadarChart>
       </td>
       <td className="actions">
         <button className="" onClick={() => {
-          setModifiedPlayer({ picture, ...data });
+          setModifiedPlayer({ id, picture, weight, height, speed, strength, endurance, ...data });
           goToNextSection();
         }}>Modifier</button>
         <button className="" onClick={() => removePlayer(data.id)}>Supprimer</button>
