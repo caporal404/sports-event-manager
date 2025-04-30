1f455f365f95b3a8478038d54f351f2b1a240dc0
caporal404 <kevincode26@gmail.com>
2025-04-30 17:11:02 +0100

    Ajout du Composant List pour afficher les listes d'informations simplifier des joueurs. Qui pourr aussi être utilisée pour afficher les équipes

---
+++ src/components/List.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import '../styles/List.css'
+
+const List = ({ id, data, onSelectItem = f => f }) => {
+  return (
+    <div className="list" id={id}>
+      {
+        data.map(item => (
+            <Item
+              key={`item-${item.id}`}
+              data={item} 
+              onSelect={onSelectItem} 
+            />
+          )
+        )
+      }
+    </div>
+  )
+}
+
+const Item = ({ data, onSelect = f => f }) => {
+  return (
+    <div className="list-item" onClick={() => onSelect(data)}>
+        <img src={data.picture} alt={data.name}/>
+        <div className="info">
+            <span className='title'>{data.name}</span>
+            <span className='text'>{data.role }</span>
+        </div>
+    </div>
+  )
+}
+
+export default List;
